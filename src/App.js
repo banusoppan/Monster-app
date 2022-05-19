86ad0e8896c6a6b60f92260dfc4a63f4e3fee86e
Banusoppan <kjksopan@gmail.com>
2022-05-19 13:01:02 +0530

    Updated ReadMe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React , { Component } from 'react';
+import { CardList } from './components/card-list/card-list.componet';
+import { Search } from './components/search/search.component'
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      name : 'Banu',
+      monsters :[],
+      searchField : ''
+    }
+    // this.handleChange = this.handleChange.bind(this)
+
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=>response.json())
+      .then(users=>this.setState({monsters:users}))
+      
+  }
+  handleChange = (e)=>{
+    this.setState({searchField:e.target.value})
+  }
+  render(){
+    const { monsters,searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster=>monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+        <h1>Monster Blog</h1>
+        
+        <Search 
+          placeholder="search monster" 
+          handleChange={this.handleChange}>
+
+        </Search>
+
+        <CardList >{filteredMonsters}</CardList>
+        
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search.component.css';
+export const Search = ({placeholder,handleChange})=>{
+    return(
+        
+            <input className="search"
+                type="search"
+                placeholder= {placeholder} 
+                onChange={handleChange}>
+                    
+            </input>
+        
+    )
+}
